715f99a1c0180f4ef2b9e5be3fb465e2ecbd4331
Furkan Emre İnce <femreince@gmail.com>
2026-02-16 14:44:39 +0300

    UI kısmı için proje oluşturuldu ve Yummy UI dosyaları projeye eklendi. ViewComponente giriş yapıldı

---
+++ ApiProjeKampi.WebUI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _HeadDefaultComponentPartial : ViewComponent // burada Default Controller ismini verir
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
